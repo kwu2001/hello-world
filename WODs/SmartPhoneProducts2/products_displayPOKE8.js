5284e1b6093c7afa8a31640cdbde0603486efcfc
kwu2001 <kailanwu@hawaii.edu>
2023-10-15 18:16:32 -1000

    commit

--- WODs/SmartPhoneProducts2/products_displayPOKE8.js
+++ WODs/SmartPhoneProducts2/products_displayPOKE8.js
@@ -92,36 +92,36 @@ let over_half=false;
 hits_span.innerHTML = hits; 
 spins_span.innerHTML = spins;
 
-//name, price, image
-    let name1 = "HTC";
-    let price1 = 40.00;
-    let image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+//item, price, image
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
 
-    let name2 = "Apple";
-    let price2 = 75.00;
-    let image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
 
-    let name3 = "Nokia";
-    let price3 = 35.00;
-    let image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
 
-    let name4 = "Samsung";
-    let price4 = 45.00;
-    let image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
 
-    let name5 = "Blackberry";
-    let price5 = 10.00;
-    let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
 
 //loop
-for (let i=1; eval ("typeof name"+1) != 'undefined'; i++) {
-document.querySelector('.main').innerHTML += `
-<section class="item" 
-onmouseover="changeClassName(this);" onclick="resetClassName(this);">
-<h2>${eval ("name"+i)}</h2>
-<p>$${eval ("price"+i)}</p>
-<img src="${eval ("image"+i)}" />
-</section>`;
+for (let i=1; eval("typeof name"+i) != 'undefined'; i++) {
+    document.querySelector('.main').innerHTML += `
+    <section class="item" 
+    onmouseover="changeClassName(this);" onclick="resetClassName(this);">
+    <h2>${eval("name"+i)}</h2>
+    <p>$${eval("price"+i)}</p>
+    <img src="${eval("image"+i)}" />
+    </section> `;
 }
 
 //footer
@@ -158,3 +158,6 @@ bottom_title.innerHTML=(`
             </tr>
         </tbody>
     </table>`);
+
+
+  
