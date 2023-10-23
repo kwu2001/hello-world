9c0600870b61cef60f1a5c42ba12bd0821e5f42c
kwu2001 <kailanwu@hawaii.edu>
2023-10-22 21:42:58 -1000

    commit

--- Lab9/lab9ex6b.js
+++ Lab9/lab9ex6b.js
@@ -1,6 +1,6 @@
 function download(url,callback) {
     setTimeout(() => {
-        // script to download the picture here
+         // Simulate downloading the picture here
         console.log(`Downloading ${url} ...`);
         picture_data = "image data:XOXOXO";
         callback(picture_data);
